171b0d7b9ca09b494576cf7e1a4a88afa5431ae1
Joshua <jdkelly2015@gmail.com>
2019-10-22 21:05:11 -0400

    reinstalled dependencies. tried to find routes

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const path = require("path");
 const mongoose = require("mongoose");
 const router = require("./Routes");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,15 +14,13 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(router);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
-// Define API routes here
-
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
